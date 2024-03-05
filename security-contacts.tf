f3d1a82d98149027afb08f6495ba3800bc92b5cb
fslef <23463958+fslef@users.noreply.github.com>
2024-03-05 14:49:33 +0100

    security contacts enhancements (#12)
    * feat: switch to AzApi for configuring security contacts
    
    * feat: add AzApi provider
    
    * fix: AzApi Provider
    
    * fix: update AzApi version
    
    * Update parent id for AzApi call
    
    * chore: move mdfc variables to the variables .ft

--- security-contacts.tf
+++ security-contacts.tf
@@ -1,12 +1,27 @@
-resource "azurerm_security_center_contact" "example" {
-  count = var.mdfc-contact-email != null ? 1 : 0
+locals {
+  notificationsByRole_state          = length(var.mdfc-alert-to-roles) > 0 || var.mdfc-alert-to-emails != null ? "On" : "Off"
+  alertNotifications_state           = var.mdfc-alert-to-emails != null || length(var.mdfc-alert-to-roles) > 0 ? "On" : "Off"
+  alertNotifications_minimalSeverity = var.mdfc-alert-minimal-severity
 
-  name  = var.mdfc-contact-name
-  email = var.mdfc-contact-email
-  phone = var.mdfc-contact-phone
+  contact_properties = {
+    notificationsByRole = {
+      state = local.notificationsByRole_state,
+      roles = var.mdfc-alert-to-roles
+    },
+    emails = var.mdfc-alert-to-emails != null ? var.mdfc-alert-to-emails : "",
+    phone  = var.mdfc-alert-to-phone != null ? var.mdfc-alert-to-phone : "",
+    alertNotifications = {
+      state           = local.alertNotifications_state,
+      minimalSeverity = local.alertNotifications_minimalSeverity
+    }
+  }
+}
 
-  alert_notifications = var.mdfc-alert-by-email
-  alerts_to_admins    = var.mdfc-alert-to-admins
-
-  
+resource "azapi_resource" "security_contact" {
+  type = "Microsoft.Security/securityContacts@2020-01-01-preview"
+  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  name = "default"
+  body = jsonencode({
+    properties = local.contact_properties
+  })
 }
--- variables.tf
+++ variables.tf
@@ -56,6 +56,34 @@ variable "mdfc_subplans" {
   nullable    = false
 }
 
+variable "mdfc-alert-to-emails" {
+  type    = string
+  default = null
+}
+
+variable "mdfc-alert-to-phone" {
+  type    = string
+  default = null
+}
+
+variable "mdfc-alert-to-roles" {
+  type    = list(string)
+  default = []
+  validation {
+    condition     = alltrue([for val in var.mdfc-alert-to-roles : contains(["AccountAdmin", "Contributor", "Owner", "ServiceAdmin"], val)])
+    error_message = "The role must be an array containing any of: 'AccountAdmin', 'Contributor', 'Owner', 'ServiceAdmin'."
+  }
+}
+
+variable "mdfc-alert-minimal-severity" {
+  type    = string
+  default = "High"
+  validation {
+    condition     = contains(["High", "Low", "Medium"], var.mdfc-alert-minimal-severity)
+    error_message = "Severity must be 'High', 'Low', or 'Medium'."
+  }
+}
+
 variable "sec_score_export_rg" {
   description = "The name of the resource group for secure score export"
   type        = string
@@ -64,35 +92,6 @@ variable "sec_score_export_rg" {
 
 variable "export_to_workspace_ID" {
   description = "The workspace ID for secure score export"
-  type        = string
-  default     = null
-}
-
-variable "mdfc-alert-by-email" {
-  type        = bool
-  default     = true
-  description = "(Optional) Enable or disable alert notifications."
-  nullable    = false
-}
-
-variable "mdfc-alert-to-admins" {
-  type        = bool
-  default     = true
-  description = "(Optional) Enable or disable alerts to admins."
-  nullable    = false
-}
-
-variable "mdfc-contact-email" {
-  type        = string
-  default     = null
-}
-
-variable "mdfc-contact-phone" {
-  type        = string
-  default     = null
-}
-
-variable "mdfc-contact-name" {
   type        = string
   default     = null
 }
